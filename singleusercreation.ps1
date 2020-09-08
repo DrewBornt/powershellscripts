31a4225b1c731e23714b161dafcd9032a4daa1eb
Drew Bornt <andrew.bornt@gmail.com>
2020-09-08 15:05:48 -0500

    Add files via upload

---
+++ singleusercreation.ps1
@@ -0,0 +1,47 @@
+﻿# Collect User Information
+$fullname = Read-Host "Enter First and Last name?"
+$password = Read-Host "Password?" -AsSecureString
+$firstname, $lastname = $fullname.Split(' ')
+$username = $fullname.Replace(' ','')
+$groups = Read-Host "What groups to assign them to?"
+$groups = $groups.Split(' ')
+
+Import-Module ActiveDirectory
+
+# Create User
+New-ADUser -Name $fullname `
+-DisplayName $fullname `
+-GivenName $firstname `
+-Surname $lastname `
+-EmailAddress "$username@home.local" `
+-SamAccountName $username `
+-Path "OU=_Domain Users, DC=HOME, DC=LOCAL" `
+-Enabled $True `
+-ChangePasswordAtLogon $True `
+-UserPrincipalName "$username@home.local" `
+-AccountPassword $password
+
+Foreach ($group in $groups)
+{
+    Add-ADGroupMember `
+    -Identity $group `
+    -Members $username
+}
+
+
+# Create User Shares and Files
+
+New-Item `
+-ItemType "directory" `
+-Path "\\FileShare01\c`$\Shares\$username"
+
+New-Item `
+-ItemType "directory" `
+-Path "\\FileShare01\c`$\Shares\Scans\$username"
+
+New-SmbShare -Name "$username" -CimSession FileShare01 -Path "c:\Shares\$username"
+
+
+
+
+# Assign License (TO COME)
